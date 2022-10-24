9e5f6ca927407d0c605e4d7485dc46e0d8ee6a6d
Garox7 <giuseppe.garozzo2@icloud.com>
2022-10-24 21:34:50 +0200

    Fix Js

--- javascript/script.js
+++ javascript/script.js
@@ -2,16 +2,17 @@ const clicTitle = document.querySelector('h1');
 const eleSquares = document.querySelector('.squares');
 
 
-for (let i = 1; i <= 1000; i++) {
-    if (i % 3 == 0) {
-        eleSquares.innerHTML += `<div class="sq fizz">${'Fizz'}</div>`;
+for (let i = 1; i <= 100; i++) {
+
+    if (i % 3 == 0 && i % 5 == 0) {
+        eleSquares.innerHTML += `<div class="sq fizzbull">FizzBull</div>`;
+
+    } else if (i % 3 == 0) {
+        eleSquares.innerHTML += `<div class="sq fizz">Fizz</div>`;
         
     } else if (i % 5 == 0) {
-        eleSquares.innerHTML += `<div class="sq bull">${'Bull'}</div>`;
+        eleSquares.innerHTML += `<div class="sq bull">Bull</div>`;
         
-    } else if ((i % 3 == 0) && (i % 5 == 0)) {
-        eleSquares.innerHTML += `<div class="sq fizzbull">${'FizzBull'}</div>`;
-
     } else {
         eleSquares.innerHTML += `<div class="sq">${i}</div>`;
 
